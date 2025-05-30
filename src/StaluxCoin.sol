838f1770cb5f7572f14ed265d1670e346099d430
George Gorzhiyev <gegorz@hotmail.com>
2025-05-29 21:49:07 -0600

    Created stalux stablecoin

---
+++ src/StaluxCoin.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+ ______  ________   ______   __       __    __  __    __ 
+ /      \|        \ /      \ |  \     |  \  |  \|  \  |  \
+|  $$$$$$\\$$$$$$$$|  $$$$$$\| $$     | $$  | $$| $$  | $$
+| $$___\$$  | $$   | $$__| $$| $$     | $$  | $$ \$$\/  $$
+ \$$    \   | $$   | $$    $$| $$     | $$  | $$  >$$  $$ 
+ _\$$$$$$\  | $$   | $$$$$$$$| $$     | $$  | $$ /  $$$$\ 
+|  \__| $$  | $$   | $$  | $$| $$_____| $$__/ $$|  $$ \$$\
+ \$$    $$  | $$   | $$  | $$| $$     \\$$    $$| $$  | $$
+  \$$$$$$    \$$    \$$   \$$ \$$$$$$$$ \$$$$$$  \$$   \$$
+                                                  
+*/
+
+contract StaluxCoin is ERC20, Ownable {
+    /*------------------------- ERRORS -------------------------*/
+
+    /*\/-\/-\/-\/-\/-\/-\/-\/-- FUNCTIONS --\/-\/-\/-\/-\/-\/-\/*/
+
+    /*------------------------- CONSTRUCTOR --------------------*/
+    constructor() ERC20("StaluxCoin", "STALUX") Ownable(msg.sender) {}
+
+    /*------------------------- EXTERNAL FUNCTIONS -------------*/
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
