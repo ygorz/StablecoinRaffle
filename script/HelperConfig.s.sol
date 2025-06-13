ff9819920d99d328795d5a951a15e76504e3170e
George Gorzhiyev <gegorz@hotmail.com>
2025-06-13 16:37:06 -0600

    Working helper config for sepolia and local chain

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,23 +5,9 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol"; // Assuming you have a LinkToken mock in your test directory
 
-contract HelperConfig is Script {
-    error HelperConfig__InvalidChainId();
-
-    struct NetworkConfig {
-        uint256 entranceFee;
-        uint256 gameDuration;
-        address priceFeed;
-        address vrfCoordinator;
-        bytes32 keyHash;
-        uint256 subscriptionId;
-        uint32 callbackGasLimit;
-    }
-
-    NetworkConfig public localNetworkConfig;
-    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
-
+abstract contract CodeConstants {
     // Chain IDs
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
@@ -34,6 +20,25 @@ contract HelperConfig is Script {
     uint96 public constant MOCK_BASE_FEE = 0.25 ether;
     uint96 public constant MOCK_GAS_PRICE_LINK = 1e9;
     int256 public constant MOCK_WEI_PER_UNIT_LINK = 4e15;
+}
+
+contract HelperConfig is Script, CodeConstants {
+    error HelperConfig__InvalidChainId();
+
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 gameDuration;
+        address priceFeed;
+        address vrfCoordinator;
+        bytes32 keyHash;
+        uint256 subscriptionId;
+        uint32 callbackGasLimit;
+        address linkToken;
+        address account;
+    }
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
 
     constructor() {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
@@ -60,8 +65,10 @@ contract HelperConfig is Script {
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
             vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
             keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
-            subscriptionId: 0,
-            callbackGasLimit: 100000
+            subscriptionId: 9642763729064717391464225939360127295978090115406467302701334453968728686934,
+            callbackGasLimit: 500000,
+            linkToken: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            account: 0x6D77695FEBa33E2e2FdD435997dC4f9Ba8bFD532
         });
     }
 
@@ -75,6 +82,7 @@ contract HelperConfig is Script {
         MockV3Aggregator priceFeedMock = new MockV3Aggregator(MOCK_PRICEFEED_DECIMALS, MOCK_PRICEFEED_INITIAL_ANSWER);
         VRFCoordinatorV2_5Mock vrfCoordinatorMock =
             new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UNIT_LINK);
+        LinkToken linkTokenMock = new LinkToken();
         vm.stopBroadcast();
 
         localNetworkConfig = NetworkConfig({
@@ -86,7 +94,9 @@ contract HelperConfig is Script {
             keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             subscriptionId: 0,
             // gas limit also doesnt matter for local mock testing
-            callbackGasLimit: 100000
+            callbackGasLimit: 500000,
+            linkToken: address(linkTokenMock),
+            account: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38 // Default sender address
         });
 
         return localNetworkConfig;
