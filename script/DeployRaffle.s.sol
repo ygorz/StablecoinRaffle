e96a746fa9ea136d592b4115d8d22c6c9cfd5865
George Gorzhiyev <gegorz@hotmail.com>
2025-06-13 16:36:42 -0600

    Fleshed out deploy script to work on sepolia or anvil

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -6,15 +6,32 @@ import {Script} from "forge-std/Script.sol";
 import {StaluxCoin} from "src/StaluxCoin.sol";
 import {StablecoinRaffle} from "src/StablecoinRaffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "script/Interactions.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external {}
+    function run() external {
+        deployStablecoinRaffle();
+    }
 
     function deployStablecoinRaffle() public returns (StaluxCoin, StablecoinRaffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
-        vm.startBroadcast();
+        if (config.subscriptionId == 0) {
+            // Create a subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            //vm.roll(block.number + 1);
+            (config.subscriptionId, config.vrfCoordinator) =
+                createSubscription.createSubscription(config.vrfCoordinator, config.account);
+
+            // Fund the subscription
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                config.vrfCoordinator, config.subscriptionId, config.linkToken, config.account
+            );
+        }
+
+        vm.startBroadcast(config.account);
         StaluxCoin staluxCoin = new StaluxCoin();
         StablecoinRaffle stablecoinRaffle = new StablecoinRaffle(
             address(staluxCoin),
@@ -28,6 +45,10 @@ contract DeployRaffle is Script {
         );
         staluxCoin.transferOwnership(address(stablecoinRaffle));
         vm.stopBroadcast();
+
+        // Add a consumer, we don't use a broadcast because it's in the addconsumer script
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(address(stablecoinRaffle), config.vrfCoordinator, config.subscriptionId, config.account);
 
         return (staluxCoin, stablecoinRaffle, helperConfig);
     }
